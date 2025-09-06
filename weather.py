12e7dc69e5f0f2b361942ae58f40fa8152f083d7
Asif Shaikh <shaikhasif694@gmail.com>
2025-09-06 23:39:53 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -44,10 +44,7 @@ class_labels = {
 
 # Sidebar for navigation
 st.sidebar.title("Navigation")
-selected = st.sidebar.selectbox(
-    "Choose a page", 
-    ("Weather Forecasting", "About App", "ML Models Used", "Dataset Info", "How It Works", "Contact")
-)
+selected = st.sidebar.selectbox("Choose a page", ["Weather Forecasting", "About App", "ML Models Used", "Dataset Info", "How It Works", "Contact"])
 
 
 # ==========================
